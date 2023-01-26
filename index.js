bff8dbf7569349642ca93af512437f1bd43934df
myungkeun <audrms3568@gmail.com>
2023-01-26 21:42:24 +0900

    login_update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-Parser')
 const maria = require('./database/connect/mariadb')
 const app = express()
-const db = maria.connect();
 const jwt = require('./jwt');
 const session = require('express-session');
 
@@ -19,14 +18,40 @@ app.post('/', function (req, res) {
   if (user_id && user_pw) {
       maria.query(sql, [user_id, user_pw], function(err, rows, fields) {
          if(err) {
-          console.log("로그인 실패")
+          const response = {
+            status: 401,
+            message: "실패!"
+          };
+          res.status(401).send(response)
          }else {
-          console.log("로그인 성공")
-          const accessToken = jwt.sign(user_id);
+          if (rows.length > 0) {
+            const accessToken = jwt.sign({"user_id" : user_id});
+            const response = {
+              status: 201,
+              message: "데이터 로딩에 성공했습니다.",
+              data: {
+                token: accessToken,
+                user_id: user_id
+              }
+            }
+            res.status(201).send(response);
+
+          } else {
+            const response = {
+              status: 401,
+              message: "로그인 실패"
+            };
+            res.status(401).send(response);
+          }
+          
          }
       });
   } else {
-  console.log("아이디 비밀번호 입력 실패")    
+    const response = {
+      status: 400,
+      message: "ID PW 입력실패"
+    };
+    res.status(400).send(response);  
   }
 });
 
@@ -38,6 +63,10 @@ app.post('/qwe', function (req, res) {
   let phone = req.body.phone;
 
   const sql = "INSERT INTO user (user_id, user_pw, nickname, phone) VALUES (?,?,?,?);"
+
+  if (user_id.length < 8) {
+    return res.send('아이디는 8글자 이상 작성해야합니다.');
+  }
 
   if (user_id && user_pw && nickname && phone) {
       maria.query(sql, [user_id, user_pw, nickname, phone], function(err, rows, fields) { 
@@ -51,12 +80,6 @@ app.post('/qwe', function (req, res) {
   console.log("아이디 비밀번호 닉네임 휴대폰번호 입력 실패")    
   }
 });
-
-// id 중복확인
-
-app.post('/',function (req,res){
-
-})
 
 
 app.listen(3000)
--- jwt.js
+++ jwt.js
@@ -5,11 +5,11 @@ const secret = process.env.SECRET;
 
 
 module.exports = {
-  sign: (user) => { // access token 발급
-    const payload = { // access token에 들어갈 payload
-      id: user.id,
-      role: user.role,
-    };
+  sign: (payload) => { // access token 발급
+    // const payload = { // access token에 들어갈 payload
+    //   id: user.id,
+    //   role: user.role,
+    // };
 
     return jwt.sign(payload, secret, { // secret으로 sign하여 발급하고 return
       algorithm: 'HS256', // 암호화 알고리즘
