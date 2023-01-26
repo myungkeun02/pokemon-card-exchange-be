0caa5096138a7a7fa68316958cd59e435ac02b9e
myungkeun <audrms3568@gmail.com>
2023-01-26 20:47:05 +0900

    save

--- database/connect/mariadb.js
+++ database/connect/mariadb.js
@@ -1,9 +1,9 @@
 const maria= require('mysql');
 const conn = maria.createConnection({
-  host     : 'localhost',
-  user     : 'audrms',
-  password : 'yss00407209',
-  database : 'pokemon'
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE
 });
 
 module.exports = conn;
